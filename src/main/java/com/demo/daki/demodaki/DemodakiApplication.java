ef887db9d0cee18e8bdd246e8c47866b76a0e7d0
thepg <eboutahir11@gmail.com>
2019-08-27 16:42:25 +0100

    commit

---
+++ src/main/java/com/demo/daki/demodaki/DemodakiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.daki.demodaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/daki/demodaki/DemodakiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.daki.demodaki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemodakiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
